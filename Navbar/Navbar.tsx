06ed94c33553a4e8616270a31a4ab072785267c8
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-05-24 01:23:25 +0530

    Augmenting the Pages

---
+++ Navbar/Navbar.tsx
@@ -0,0 +1,125 @@
+/** @format */
+
+import { ReactNode } from 'react';
+import {
+	Box,
+	Flex,
+	Avatar,
+	Link,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+	useColorMode,
+	Center,
+	useBreakpointValue,
+	IconButton,
+	HStack,
+} from '@chakra-ui/react';
+import {
+	MoonIcon,
+	SunIcon,
+	HamburgerIcon,
+	CloseIcon,
+	ChevronDownIcon,
+	ChevronRightIcon,
+} from '@chakra-ui/icons';
+
+const Links = ['Dashboard', 'Projects', 'Team'];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+	<Link
+		px={2}
+		py={1}
+		rounded={'md'}
+		_hover={{
+			textDecoration: 'none',
+			bg: useColorModeValue('gray.200', 'gray.700'),
+		}}
+		href={'#'}>
+		{children}
+	</Link>
+);
+
+export default function Nav() {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+				<Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+					<IconButton
+						size={'md'}
+						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+						aria-label={'Open Menu'}
+						display={{ md: 'none' }}
+						onClick={isOpen ? onClose : onOpen}
+					/>
+					<HStack spacing={8} alignItems={'center'}>
+						<Box>Logo</Box>
+						<HStack
+							as={'nav'}
+							spacing={4}
+							display={{ base: 'none', md: 'flex' }}>
+							{Links.map((link) => (
+								<NavLink key={link}>{link}</NavLink>
+							))}
+						</HStack>
+					</HStack>
+					<Stack
+						direction={'row'}
+						flex={{ base: 2, md: 0 }}
+						justify={'flex-end'}
+						spacing={6}></Stack>
+
+					<Flex alignItems={'center'}>
+						<Stack direction={'row'} spacing={7}>
+							<Button>Button 🤔🤔🤔</Button>
+
+							<Button onClick={toggleColorMode}>
+								{colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+							</Button>
+
+							<Menu>
+								<MenuButton
+									as={Button}
+									rounded={'full'}
+									variant={'link'}
+									cursor={'pointer'}
+									minW={0}>
+									<Avatar
+										size={'sm'}
+										src={'https://avatars.dicebear.com/api/male/username.svg'}
+									/>
+								</MenuButton>
+								<MenuList alignItems={'center'}>
+									<br />
+									<Center>
+										<Avatar
+											size={'3xl'}
+											src={'https://avatars.dicebear.com/api/male/username.svg'}
+										/>
+									</Center>
+									<br />
+									<Center>
+										<p>Username</p>
+									</Center>
+									<br />
+									<MenuDivider />
+									<MenuItem>Your Servers</MenuItem>
+									<MenuItem>Account Settings</MenuItem>
+									<MenuItem>Logout</MenuItem>
+								</MenuList>
+							</Menu>
+						</Stack>
+					</Flex>
+				</Flex>
+			</Box>
+		</>
+	);
+}
